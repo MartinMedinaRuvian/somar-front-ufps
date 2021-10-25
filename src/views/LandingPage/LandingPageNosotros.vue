7c4ebd3c0ad9860a29799b4bdf431c9538c97915
JohnnyQuintero16 <johnnyaquintero@gmail.com>
2021-10-24 23:26:37 -0500

    modificando imagen

--- src/views/LandingPage/LandingPageNosotros.vue
+++ src/views/LandingPage/LandingPageNosotros.vue
@@ -9,7 +9,7 @@
             <img src="../../assets/martin.png" alt="foto-martin" width="80%">
           </div>
           <div class="card-body">
-            <h5>Martin de Jesus Medina Ruvian</h5>
+            <h5><b>Martin de Jesus Medina Ruvian</b></h5>
             <p>Desarrollador frontend</p>
           </div>
         </div>
@@ -17,10 +17,10 @@
       <div class="col-md-4">
         <div class="card">
           <div class="card-header">
-            <img src="" alt="">
+            <img src="../../assets/johnny.jpg" alt="foto-johnny" width="60.5%">
           </div>
           <div class="card-body">
-            <h5>Johnny Alexander Quintero</h5>
+            <h5><b>Johnny Alexander Quintero</b></h5>
             <p>Desarrollador backend</p>
           </div>
         </div>        
@@ -31,7 +31,7 @@
             <img src="" alt="">
           </div>
           <div class="card-body">
-            <h5>Brayam Andrey Oliveros</h5>
+            <h5><b>Brayam Andrey Oliveros</b></h5>
             <p>Desarrollador fullstack</p>
           </div>
         </div>           
